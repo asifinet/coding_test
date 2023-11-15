953a0c8ac480d164327000b008e9cae08ccaec36
Muhammad Asif <asif_inet@hotmail.com>
2023-11-15 20:05:54 +0500

    Initial Commit coding_test TransactionFetcher

---
+++ src/main/java/com/smallworld/TransactionDataFetcher.java
@@ -0,0 +1,101 @@
+package com.smallworld;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smallworld.data.Transaction;
+
+public class TransactionDataFetcher {
+
+    private List<Transaction> transactions;
+
+    // Constructor to initialize transactions from the JSON file
+ 
+    public TransactionDataFetcher(InputStream inputStream) throws IOException {
+        this.transactions = loadTransactionsFromStream(inputStream);
+    }
+    
+    
+    private List<Transaction> loadTransactionsFromStream(InputStream inputStream) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(inputStream);
+
+        List<Transaction> transactionList = new ArrayList<>();
+
+        if (rootNode.isArray()) {
+            Iterator<JsonNode> elements = rootNode.elements();
+
+            while (elements.hasNext()) {
+                JsonNode transactionNode = elements.next();
+                Transaction transaction = objectMapper.treeToValue(transactionNode, Transaction.class);
+                transactionList.add(transaction);
+            }
+        }
+
+        return transactionList;
+    }
+    
+    
+    // Method to get a list of unique transaction identifiers (mtn)
+    public List<Long> getUniqueTransactionIds() {
+        // Implement logic to extract unique transaction IDs
+        // You can use Java streams to achieve this
+        return transactions.stream()
+                .map(Transaction::getMtn)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    // Method to get the total transaction amount for a given transaction ID
+    public double getTotalAmountForTransaction(long transactionId) {
+        // Implement logic to calculate total amount for a given transaction ID
+        // You can use Java streams and filter by transaction ID
+        return transactions.stream()
+                .filter(transaction -> transaction.getMtn() == transactionId)
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+    }
+
+    // Method to get a map of transaction IDs to their total amounts
+    public Map<Long, Double> getTotalAmountsForAllTransactions() {
+        // Implement logic to calculate total amounts for all transactions
+        // You can use Java streams to group by transaction ID and sum amounts
+        return transactions.stream()
+                .collect(Collectors.groupingBy(
+                        Transaction::getMtn,
+                        Collectors.summingDouble(Transaction::getAmount)
+                ));
+    }
+
+    // Method to get the count of transactions with unresolved issues
+    public long getCountOfTransactionsWithUnresolvedIssues() {
+        // Implement logic to count transactions with unresolved issues
+        // You can use Java streams to filter by issueSolved
+        return transactions.stream()
+                .filter(transaction -> transaction.getIssueId() != null && !transaction.isIssueSolved())
+                .count();
+    }
+
+    // Method to get the average age of senders with resolved issues
+    public double getAverageAgeOfSendersWithResolvedIssues() {
+        // Implement logic to calculate the average age of senders with resolved issues
+        // You can use Java streams to filter by issueSolved and map to sender age
+        return transactions.stream()
+                .filter(transaction -> transaction.getIssueId() != null && transaction.isIssueSolved())
+                .mapToDouble(Transaction::getSenderAge)
+                .average()
+                .orElse(0.0); // Return 0 if there are no transactions with resolved issues
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }    
+
+}
